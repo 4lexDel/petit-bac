13abb1ebf141f664d10d552f4bc6cd401f426d70
Alexandre DELORME <alexandre.delorme@viacesi.fr>
2023-08-22 17:18:03 +0200

    style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const categories = [
     "Animaux",
-    "Prénoms (Filles, Garçons)",
+    "Prénoms Garçons",
+    "Prénoms Filles",
     "Couleurs",
     "Métiers",
     "Marques",
